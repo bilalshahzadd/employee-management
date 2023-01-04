dc1680afe011a92434ca1b8eb7d07ff523bded80
bilalshahzadd <bilalshahzad521@gmail.com>
2023-01-04 18:26:50 +0500

    form update

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ include('partials/header.php');
             <input type="text" class="form-control" id="name" aria-describedby="name" name="name">
         </div>
         <div class="mb-3">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" class="form-control" id="email" name="email">
+            <label for="email" class="form-label">Email:</label> 
+            <input type="email" class="form-control" id="email" name="email"> 
         </div>
         <div class="mb-3">
             <label for="num" class="form-label">Phone Number:</label>
