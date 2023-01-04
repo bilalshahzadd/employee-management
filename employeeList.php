ac7ebd38302fb273892c4e10c6e5e7ed5025bc2d
bilalshahzadd <bilalshahzad521@gmail.com>
2023-01-04 18:26:29 +0500

    employee list file

---
+++ employeeList.php
@@ -0,0 +1,10 @@
+<?php
+include('partials/header.php');
+?>
+<div class="container my-4">
+    <h1 class="display-6">Employee List Will Be Displayed Here</h1>
+</div>
+//! ADD EMPLOYEE LIST
+<?php
+include('partials/footer.php');
+?>
