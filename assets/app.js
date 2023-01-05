0f0f4ee44533f2a30ea380fda0619aa5782fb211
bilalshahzadd <bilalshahzad521@gmail.com>
2023-01-05 18:16:08 +0500

    bug fixes

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,8 @@
-edits = document.getElementsByClassName('edits');
+const edits = document.getElementsByClassName('edits');
+console.log(edits);
+console.log("Im loading");
+Array.from(edits).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        console.log("Script is working");
+    })
+})
